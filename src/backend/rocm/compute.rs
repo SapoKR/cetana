fe1556a147daf84ef678f24c0ec4d37ef50eda92
SapoKR <phmi1621@naver.com>
2025-01-07 10:40:00 +0900

    HIP partial content added!
    Added compute.rs
    Modified core.rs&mod.rs

---
+++ src/backend/rocm/compute.rs
@@ -0,0 +1,28 @@
+
+
+#[link(name="rocm")]
+extern "C" {
+    fn hipMalloc(ptr: *mut *mut std::ffi::c_void, size: usize) -> i32;
+    fn hipFree(ptr: *mut std::ffi::c_void) -> i32;
+    fn hipMemcpy(
+        dst: *mut std::ffi::c_void,
+        src: *const std::ffi::c_void,
+       sizeBytes: usize,
+       kind: hipMemcpyKind,
+    )
+    fn hipMemset(ptr: *mut std::ffi::c_void, value: i32, sizeBytes: usize) -> i32;
+    fn hipDeviceSynchronize() -> i32;
+}
+
+#[repr(C)]
+#[derive(Debug, Clone, Copy)]
+pub enum hipMemcpyKind {
+    hipMemcpyHostToHost,
+    hipMemcpyHostToDevice,
+    hipMemcpyDeviceToHost,
+    hipMemcpyDeviceToDevice,
+    hipMemcpyDefault,
+    hipMemcpyDeviceToDeviceNoCU,
+}
+
+const HIP_SUCCESS: i32 = 0;
--- src/backend/rocm/core.rs
+++ src/backend/rocm/core.rs
@@ -1,7 +1,22 @@
+#[link(name = "rocm")]
+extern "C" {
+    fn hipGetDeviceProperties(prop: *mut rocmDeviceProp, device: i32) -> i32;
+    fn hipDeviceGetName(name: *mut i8, len: i32, device: i32) -> i32;
+    fn hipDeviceGetAttribute(value: *mut i32, attr: i32, device: i32) -> i32;
+    fn hipMemGetInfo(free: *mut usize, total: *mut usize) -> i32;
+    fn hipSetDevice(device: i32) -> i32;
+    fn hipDeviceSynchronize() -> i32;
+    fn hipGetDeviceCount(count: *mut i32) -> i32;
+}
+
+/*struct hipUUID {
+    bytes: [i8, 16]
+}*/
+
 #[repr(C)]
 struct rocmDeviceProp {
     name: [i8; 256],
-    //uuid: hipUUID ?
+    //uuid: hipUUID,
     luid: [i8; 8],
     //luidDeviceNodeMask ?
     totalGlobalMem: usize,
@@ -17,4 +32,12 @@ struct rocmDeviceProp {
     major: i32,
     minor: i32,
     // ...
+}
+
+const HIP_SUCCESS: i32 = 0;
+const HIP_DEVICE_ATTR_COMPUTE_CAPBILITY_MAJOR: i32 = 25
+const HIP_DEVICE_ATTR_COMPUTE_CAPBILITY_MINOR: i32 = 63
+
+impl RocmDevice {
+    pub fn new(device_id: i32) -> Result<Self,
 }
